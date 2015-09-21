09d2eb442561014141b09a4e3e566189581bb078
James Youn <jamesyoun710@gmail.com>
2015-09-20 19:22:39 -0700

    Config file edits for heroku

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module CustEvent
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # to remove precompile errors
+    config.assets.initialize_on_precompile = false
   end
 end
